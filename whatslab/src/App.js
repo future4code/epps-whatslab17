7c05107c52d81eb7cc6131e03f3998c3630b3aff
franciane-brito <francianebrito99@gmail.com>
2021-01-04 02:49:12 -0300

    Finalizando desafios

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import Styled from 'styled-components'
 import MensagemFinal from './MensagemFinal.js';
+
+
+const ContainerMensagens = Styled.div`
+background-color: #9F9F9F;
+display: flex;
+flex-direction: column-reverse;
+justify-content: flex-start;
+height: 100vh;
+width: 80vw;
+border: solid #333 1px;
+
+`
 
 
 class App extends React.Component {
@@ -22,14 +35,41 @@ class App extends React.Component {
         const novaMensagem = {
           nomeUsuario: this.state.valorInputNomeUsuario,
           mensagemUsuario: this.state.valorInputMensagemUsuario
-  };
 
+  };
+if(this.state.valorInputNomeUsuario !== '' && this.state.valorInputMensagemUsuario !== ''){
   const mensagensAtualizadas = [...this.state.mensagens, novaMensagem];
 
   this.setState({ mensagens: mensagensAtualizadas });
   this.state.valorInputNomeUsuario = ''
   this.state.valorInputMensagemUsuario = ''
+} else {
+  alert('Preencha seu nome e mensagem desejada')
+}
       }
+
+    adicionaMensagemEnter = (e) => {
+        if(e.key === "Enter") {
+          this.adicionaMensagem()
+      }
+    }
+/* REMOVER MENSAGEM */
+    removerMensagem = (nome) => {
+      console.log("Removendo mensagem")
+
+      const mensagensAtuais = this.state.mensagens
+      if(window.confirm("Tem certeza que quer apagar essa mensagem") === true){
+      const novaLista = mensagensAtuais.filter((mensagem) => {
+        return mensagem.nomeUsuario !== nome; 
+
+    })
+    this.setState({ mensagens: novaLista });
+  }
+  }
+  
+
+/* FIM REMOVER MENSAGEM */
+
 
 
   onChangeInputNomeUsuario = (event) => {
@@ -41,35 +81,46 @@ class App extends React.Component {
   };
   
   
+
+  
   render() {
     const listaDeMensagens = this.state.mensagens.map((mensagem) => {
+    
       return (
+        <div onDoubleClick={() => this.removerMensagem(mensagem.nomeUsuario)}>
         <MensagemFinal
         nomeUsuario = {mensagem.nomeUsuario}
         mensagemUsuario = {mensagem.mensagemUsuario}
-       />
+        />
+        </div>
+
       );
     });
 
     return (
-      <div>
+      <ContainerMensagens cor={this.props.nomeUsuario}>
         <div>
           <input
             value={this.state.valorInputNomeUsuario}
             onChange={this.onChangeInputNomeUsuario}
             placeholder={"Usuário"}
+            onKeyDown={this.adicionaMensagemEnter}
+
           />
           <input
           value={this.state.valorInputMensagemUsuario}
           onChange={this.onChangeInputMensagemUsuario}
             placeholder={"Mensagem"}
+            onKeyDown={this.adicionaMensagemEnter}
             />
             
-            
+
+
+
             <button onClick={this.adicionaMensagem}>Enviar</button>
             </div>
             <div className={'container-mensagens'}>{listaDeMensagens}</div>
-            </div>
+            </ContainerMensagens>
             );
           }
           
@@ -79,3 +130,5 @@ class App extends React.Component {
       
 
 export default App;
+
+
